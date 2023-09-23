fc09c1f428be62e6440d9baeb55cc5532ef9c90b
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-09-23 09:45:23 -0300

    feat: used theme provider and post page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { Post } from '@/pages/Post'
+import { GlobalStyle } from '@/styles/global'
+import { defaultTheme } from '@/styles/themes/default'
 import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
 
 export function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
 			<GlobalStyle />
 
-			<h1>Hello World</h1>
+			<Post />
 		</ThemeProvider>
 	)
 }
